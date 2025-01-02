35b0fbedea8131113a79147c1f1af2c69b2188b1
David Anderson <215816+davidryal@users.noreply.github.com>
2025-01-02 16:44:30 +0000

    Restored to '036ad14ac5433709a0bb56063f50f1974f342198'
    Replit-Restored-To:036ad14ac5433709a0bb56063f50f1974f342198

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 export default defineConfig({
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
-  server: {
-    hmr: {
-      overlay: false
-    }
-  },
   resolve: {
     alias: {
       "@db": path.resolve(__dirname, "db"),
